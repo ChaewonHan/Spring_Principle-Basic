f7bc390e7114ac0dd1ade679d64a56a73a14aa0e
ChaewonHan <chwonhan00@gmail.com>
2021-06-03 14:44:02 +0900

    App config 추가하여 관심사 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    // ! 의존관계가 인터페이스에만 의존하지 않고 구현체에도 의존하고 있다 => SOLID의 DIP 규칙 위반
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
