ad0390a6608416be3cc871c2a6106b07557e0269
ChaewonHan <chwonhan00@gmail.com>
2021-06-07 15:16:41 +0900

    스프링으로 전환, 스프링 빈 조회 추가

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -6,22 +6,28 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // => 설정(구성) 정보
 public class AppConfig {
 
-    // 생성자 주입
+    @Bean // => 반환된 객체를 스프링 컨테이너에 등록한다, 메서드명을 스프링 빈의 이름으로 사용
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
